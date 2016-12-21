0e2d18d3647ce1d503bb36496a884c3bb3fdd117
Arguelles Rojas, Diego J <diego.arguellesrojas@one.verizon.com>
2016-12-21 14:09:32 -0500

    sendPushMessage method can be called by post request without subscribing to Stomp server

--- src/main/java/com/verizon/api/websocks/PushMessageController.java
+++ src/main/java/com/verizon/api/websocks/PushMessageController.java
@@ -1,53 +1,40 @@
 package com.verizon.api.websocks;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-
+import com.verizon.models.Message;
+import com.verizon.models.PushMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import com.verizon.models.Message;
-import com.verizon.models.PushMessage;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Controller
 public class PushMessageController {
 
     private static Logger LOGGER = LoggerFactory.getLogger(PushMessageController.class);
 
-//    private TokenStorageService tokenStorageService;
-
     private SimpMessagingTemplate simpMessagingTemplate;
-
-//    private final static String headerName = "X-Auth";
 
     @Autowired
     public PushMessageController(SimpMessagingTemplate simpMessagingTemplate) {
         this.simpMessagingTemplate = simpMessagingTemplate;
-//        this.tokenStorageService = tokenStorageService;
     }
 
-    @MessageMapping("/push-to/{channel}")
-    public void sendPushMessageToReceiverByToken(@DestinationVariable String channel, Message message) {
-        // Sender sender = getSenderFromToken(senderToken);
-//        String tokenInRedis = tokenStorageService.findTokenBySenderCompany(sender.getCompany());
-//        boolean tokenActive = tokenInRedis.equals(senderToken);
-//        Map<String, Boolean> messageHeaders = new HashMap<>();
-//        if (tokenActive) {
-            LOGGER.info("Valid token, sending message to channel...");
-//            messageHeaders.put("alive", true);
-            sendMessage(channel, new PushMessage(message));
-//        } else {
-//            LOGGER.debug("Token expired for channel..." + sender.getChannel());
-//            messageHeaders.put("alive", false);
-//            sendMessage(sender.getChannel(), new PushMessage(null, null), messageHeaders);
-//        }
+    @PostMapping("/notification/push-to/{channel}")
+    public ResponseEntity<PushMessage> sendPushMessageToReceiverByToken(@PathVariable String channel, @RequestBody Message message) {
+        LOGGER.info("Valid token, sending message to channel...");
+        PushMessage pushMessage = new PushMessage(message);
+        sendMessage(channel, pushMessage);
+        return ResponseEntity.ok(pushMessage);
     }
+
 
     private void sendMessage(String toChannel, Object message) {
         this.simpMessagingTemplate.convertAndSend("/receiver/push-client/".concat(toChannel), message);
